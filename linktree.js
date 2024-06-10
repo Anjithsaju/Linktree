cae8f257deb6f3f3d979cfd44bf1780033fa9c69
Anjithsaju <anjithsaju123@gmail.com>
2024-06-10 20:06:07 +0530

    Add files via upload

---
+++ linktree.js
@@ -0,0 +1,15 @@
+const btn1=document.getElementById('1');
+btn1.addEventListener('click',e=>{
+  location.replace('https://www.instagram.com/anjith_saju?igsh=MTB3Y3B5dW1pdTJ2Nw==')});
+  const btn2=document.getElementById('2');
+  btn2.addEventListener('click',e=>{
+    alert("not defined")});
+    const btn3=document.getElementById('3');
+btn3.addEventListener('click',e=>{
+    alert("not defined")});
+  const btn4=document.getElementById('4');
+btn4.addEventListener('click',e=>{
+    alert("not defined")});
+  const btn5=document.getElementById('5');
+btn5.addEventListener('click',e=>{
+    alert("not defined")});
